3be2ee8ab2b98890e3a453a0bd471a0f28408d05
一萱 <jjps30516@gmail.com>
2023-12-18 17:36:12 +0800

    feat: home

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -15,22 +15,30 @@ import { FormsModule } from "@angular/forms";
 export class HomeComponent {
   forum = ['Dcard', 'Mobile01', 'PTT',];
   selected_forum: any;
-
-  dcard_type = ['美妝', '感情', '閒聊', '健康', '美食', '旅遊']
-  mobile01_type = ['閒聊與趣味', '兩性與感情', '女人心事', '投資理財綜合', '機車消費經驗分享']
-  ptt_type = ['Gossiping', 'C_Chat', 'WomenTalk', 'HatePolitics', 'joke', 'StupidClown', 'e-shopping']
+  word_limit = ['100', '300', '600', '1000']
+  selected_word_limit: any;
+  board: any[] = [];
+  selected_board: any;
+  dcard_board = ['美妝', '感情', '閒聊', '健康', '美食', '旅遊']
+  mobile01_board = ['閒聊與趣味', '兩性與感情', '女人心事', '投資理財綜合', '機車消費經驗分享']
+  ptt_board = ['Gossiping', 'C_Chat', 'WomenTalk', 'HatePolitics', 'joke', 'StupidClown', 'e-shopping']
+  type = ['商品體驗開箱文', '競品內容比較文', '爆紅事件議題文', '分享故事置入產品']
   selected_type: any;
+  style = ['溫馨感人', 'KUSO感人', '理性嚴肅', '誇張幻想']
+  selected_style: any;
+  sponsorship = ['輕', '中', '高']
+  selected_sponsorship: any;
 
   getTypes() {
     switch (this.selected_forum) {
       case 'Dcard':
-        return this.dcard_type;
+        return this.board = this.dcard_board;
       case 'Mobile01':
-        return this.mobile01_type;
+        return this.board = this.mobile01_board;
       case 'PTT':
-        return this.ptt_type;
+        return this.board = this.ptt_board;
       default:
-        return ['尚未選擇論壇'];
+        return this.board = ['尚未選擇論壇'];
     }
   }
 
