6768b0f5ce29ac094ca3666a651233d9be354f10
一萱 <jjps30516@gmail.com>
2023-12-21 13:41:00 +0800

    feat: toolbar

---
+++ src/app/view/component/toolbar/toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { PRIMENG_MODULES } from "../../../share/primeng";
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [
+    PRIMENG_MODULES
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}
