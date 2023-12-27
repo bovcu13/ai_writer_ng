59776a280dce786bce779ece9de7902729296fac
一萱 <jjps30516@gmail.com>
2023-12-27 14:46:38 +0800

    style: settings-menu

--- src/app/share/data/settings-menu.ts
+++ src/app/share/data/settings-menu.ts
@@ -1,21 +1,21 @@
 export const settingsMenu = [
   {
-    icon: 'text-500 fa-solid fa-gear',
+    icon: 'text-lg text-700 fa-solid fa-gear',
     name: '帳號設定',
     route: '/settings/profile',
   },
   {
-    icon: 'text-500 fa-solid fa-clock-rotate-left',
+    icon: 'text-lg text-700 fa-solid fa-clock-rotate-left',
     name: '產文歷史記錄',
     route: '/settings/outputHistory',
   },
   {
-    icon: 'text-500 fa-solid fa-solid fa-shield-cat',
+    icon: 'text-lg text-700 fa-solid fa-solid fa-shield-cat',
     name: '安全性',
     route: '/settings/safety',
   },
   {
-    icon: 'text-500 fa-solid fa-paw',
+    icon: 'text-lg text-700 fa-solid fa-paw',
     name: '訂閱方案',
     route: '/settings/subscription',
   },
