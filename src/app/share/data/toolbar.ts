b911356433bb08af19a0814ba73aa18e4d33fb6e
一萱 <jjps30516@gmail.com>
2024-01-15 13:32:37 +0800

    refactor: toolbar router

--- src/app/share/data/toolbar.ts
+++ src/app/share/data/toolbar.ts
@@ -1,10 +1,12 @@
 export const menu = [
   {
     label: '文章生成',
+    routerLink: '/home',
     icon: 'pi pi-fw pi-pencil',
   },
   {
     label: '回文生成',
+    routerLink: '/reply',
     icon: 'pi pi-fw pi-pencil',
   },
 ]
