03ce5174fa2c0c4f6c05cc0b9c42836002edceef
一萱 <jjps30516@gmail.com>
2024-01-23 15:16:11 +0800

    feat: env

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  API_URL: "https://fmp.t.api.jinher-net.com/fms",
+  production: true,
+  recaptcha: {
+    siteKey: '6LeVYMYnAAAAAE4srV95DGTL-HoKyygRetgeMZ-B'
+  },
+};
