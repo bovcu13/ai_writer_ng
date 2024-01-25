6f0a0c8495cfb0ed7827a34ac6eb5b2e570f9523
一萱 <jjps30516@gmail.com>
2024-01-25 16:19:47 +0800

    test: HttpClient with Interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -10,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient()
+    // provideHttpClient(withInterceptors([httpInterceptorProviders]))
   ]
 };
