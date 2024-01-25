bafba29234cff5d6134dffcdea31441bce75aa91
一萱 <jjps30516@gmail.com>
2024-01-25 17:00:38 +0800

    feat: added ** route and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,29 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from "./view/component/login/login.component";
+import { authGuard } from "./share/auth.guard";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: '', loadChildren: () => import('./view/container/container.routes').then(m => m.routes) },
-  { path: 'settings', loadChildren: () => import('./view/settings/settings.routes').then(m => m.routes) },
-  { path: 'admin', loadChildren: () => import('./admin/admin/admin.routes').then(m => m.routes) },
+  {
+    path: '',
+    loadChildren: () => import('./view/container/container.routes').then(m => m.routes),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./view/settings/settings.routes').then(m => m.routes),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin/admin.routes').then(m => m.routes),
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
 ];
