2abcf570d8e6c3ea1725e64fc8fae9542c708d05
一萱 <jjps30516@gmail.com>
2024-01-26 15:37:18 +0800

    refactor: rename sessionStorage Item user -> auth-user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,11 +9,11 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return window.sessionStorage.getItem('user') !== null;
+    return window.sessionStorage.getItem('auth-user') !== null;
   }
 
   getUserRole() {
-    return window.sessionStorage.getItem('user');
+    return window.sessionStorage.getItem('auth-user');
   }
 
   logout() {
