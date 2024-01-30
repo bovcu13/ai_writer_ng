ad2b856299109037cb14487544e55b13d183800c
一萱 <jjps30516@gmail.com>
2024-01-30 15:24:48 +0800

    feat: getUser

--- src/app/admin/member-mgmt/member-mgmt.component.ts
+++ src/app/admin/member-mgmt/member-mgmt.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PRIMENG_MODULES } from "../../share/primeng";
 import { member } from "../../share/data/member";
-import { NgClass, NgIf } from "@angular/common";
+import { DatePipe, NgClass, NgIf } from "@angular/common";
 import { Router } from "@angular/router";
 import { FormsModule } from "@angular/forms";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-member-mgmt',
@@ -12,17 +13,19 @@ import { FormsModule } from "@angular/forms";
     PRIMENG_MODULES,
     NgClass,
     NgIf,
-    FormsModule
+    FormsModule,
+    DatePipe
   ],
   templateUrl: './member-mgmt.component.html',
   styleUrl: './member-mgmt.component.scss'
 })
 export class MemberMgmtComponent implements OnInit {
-  member = member.map(m =>
-    (
-      { ...m, last_name: m.name[0] }
-    ));
-  selectedMembers: any[] = [];
+  // member = member.map(m =>
+  //   (
+  //     { ...m, last_name: m.name[0] }
+  //   ));
+  memberData: any[] = []
+  selectedMembers: any[] = []
   memberPoint: any
   point: any
 
@@ -34,12 +37,33 @@ export class MemberMgmtComponent implements OnInit {
   isActiveActive: boolean = false;
   isActiveInactive: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(
+    private userServ: UserService,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
-    this.countActiveMembers(this.member);
-    this.showActive();
+    this.getMembers();
+  }
+
+  getMembers() {
+    this.userServ.getAllUserRequest().subscribe({
+      next: (res) => {
+        this.memberData  = res.body.users.map((m: any) => {
+          return (
+            { ...m, last_name: m.name[0] }
+          );
+        });
+        console.log('this.memberData', this.memberData)
+
+        this.countActiveMembers(this.memberData);
+        this.showActive();
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
 
   // 進入會員詳細
@@ -81,19 +105,19 @@ export class MemberMgmtComponent implements OnInit {
 
   // 禁用會員
   disableMember(member: any) {
-    const index = this.member.findIndex(m => m.id === member.id);
+    const index = this.memberData.findIndex(m => m.id === member.id);
     if (index !== -1) {
-      this.member[index].active = false;
-      this.countActiveMembers(this.member);
+      this.memberData[index].active = false;
+      this.countActiveMembers(this.memberData);
     }
   }
 
   // 啟用會員
   enableMember(member: any) {
-    const index = this.member.findIndex(m => m.id === member.id);
+    const index = this.memberData.findIndex(m => m.id === member.id);
     if (index !== -1) {
-      this.member[index].active = true;
-      this.countActiveMembers(this.member);
+      this.memberData[index].active = true;
+      this.countActiveMembers(this.memberData);
     }
   }
 
