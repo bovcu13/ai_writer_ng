bb1c7ddf6674ad91a608719e722edcd7357a5f67
一萱 <jjps30516@gmail.com>
2024-05-07 11:28:09 +0800

    feat: 解除頁面進入限制
    canActivate: [authGuard]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,17 +9,17 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./view/container/container.routes').then(m => m.routes),
-    canActivate: [authGuard]
+
   },
   {
     path: 'settings',
     loadChildren: () => import('./view/settings/settings.routes').then(m => m.routes),
-    canActivate: [authGuard]
+
   },
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin/admin.routes').then(m => m.routes),
-    canActivate: [authGuard]
+
   },
   {
     path: '**',
